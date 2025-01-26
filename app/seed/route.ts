c4fd7eab3ae9515c77026d2d0fb998a1fd0cf93f
saad fatihi <saad.fatihi-ext@kering.com>
2025-01-26 13:37:26 +0100

    Use real data

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -103,8 +103,7 @@
 
 export async function GET() {
   return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
+    message: "Database already seeded",
   });
   // try {
   //   await client.sql`BEGIN`;
